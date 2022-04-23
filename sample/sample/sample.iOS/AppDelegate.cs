48cc64297ccb28a66966ffff9fe9e2acdf5716f6
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2022-04-23 21:17:15 +0200

    Setup Info.plist

--- sample/sample/sample.iOS/AppDelegate.cs
+++ sample/sample/sample.iOS/AppDelegate.cs
@@ -13,7 +13,6 @@ namespace sample.iOS
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
-        //
         // This method is invoked when the application has loaded and is ready to run. In this 
         // method you should instantiate the window, load the UI into it and then make the window
         // visible.
